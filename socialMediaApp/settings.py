bdec2ab658212e8fef6dbdb3557a871b65f37b77
marcusg <marcusg@bixly.com>
2016-07-13 14:12:25 -0700

    changed to postgres locally

--- socialMediaApp/settings.py
+++ socialMediaApp/settings.py
@@ -81,6 +81,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'myproject',
+    #     'USER': 'myprojectuser',
+    #     'PASSWORD': 'Password123',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
 }
 
 
